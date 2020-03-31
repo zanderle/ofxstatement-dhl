c224b89a2876f7534482b270441a62cfffb20190
Žan Anderle <zan.anderle@gmail.com>
2020-03-31 15:04:50 +0200

    Update for the new csv format

--- src/ofxstatement/plugins/dhl.py
+++ src/ofxstatement/plugins/dhl.py
@@ -7,12 +7,33 @@ from ofxstatement.statement import StatementLine, BankAccount
 
 SIGNATURES = [
     "Valuta;Datum valute;Datum knjiženja;ID transakcije;Št. za reklamacijo;Prejemnik / Plačnik;Breme;Dobro;Referenca plačnika;Referenca prejemnika;Opis prejemnika",  # Oct 2018
+    "Datum valute;Datum knjiženja;ID transakcije;Št. za reklamacijo;Prejemnik / Plačnik;Breme;Dobro;Referenca plačnika;Referenca prejemnika;Opis prejemnika",  # Mar 2020
 ]
 
 
 class DhlCSVStatementParser(CsvStatementParser):
 
     date_format = "%d.%m.%Y"
+    OLD_MAP = {
+        'paid_out': 6,
+        'paid_in': 7,
+        'date': 1,
+        'reference': 3,
+        'payee': 5,
+        'memo': 10,
+    }
+    NEW_MAP = {
+        'paid_out': 5,
+        'paid_in': 6,
+        'date': 0,
+        'reference': 2,
+        'payee': 4,
+        'memo': 9,
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.line_map = kwargs.pop('line_map')
+        super().__init__(*args, **kwargs)
 
     def split_records(self):
         return csv.reader(self.fin, delimiter=';', quotechar='"')
@@ -26,21 +47,21 @@ class DhlCSVStatementParser(CsvStatementParser):
     def parse_record(self, line):
         try:
             stmt_line = StatementLine()
-            stmt_line.date = self.parse_datetime(line[1].strip())
+            stmt_line.date = self.parse_datetime(line[self.line_map['date']].strip())
 
             # Amount
-            paid_out = -self.parse_amount(line[6])
-            paid_in = self.parse_amount(line[7])
+            paid_out = -self.parse_amount(line[self.line_map['paid_out']])
+            paid_in = self.parse_amount(line[self.line_map['paid_in']])
             stmt_line.amount = paid_out or paid_in
 
-            reference = line[3].strip()
+            reference = line[self.line_map['reference']].strip()
             trntype = False
 
             if not trntype:
                 trntype = 'POS'  # Default: Debit card payment
 
-            stmt_line.payee = line[5]
-            stmt_line.memo = line[10]
+            stmt_line.payee = line[self.line_map['payee']]
+            stmt_line.memo = line[self.line_map['memo']]
 
             return stmt_line
         except Exception as e:
@@ -55,7 +76,11 @@ class DHLPlugin(Plugin):
         while line != '':
             line = line.strip()
             if line in SIGNATURES:
-                parser = DhlCSVStatementParser(f)
+                if line == SIGNATURES[0]:
+                    line_map = DhlCSVStatementParser.OLD_MAP
+                else:
+                    line_map = DhlCSVStatementParser.NEW_MAP
+                parser = DhlCSVStatementParser(f, line_map=line_map)
                 if 'account' in self.settings:
                     parser.statement.account_id = self.settings['account']
                 else:
